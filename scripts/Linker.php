3ccdeb1b52c7870edbc1df909d4e5ed6e2b24078
Takashi Kanemoto <t-kanemoto@quartet-mail.com>
2014-08-15 16:53:51 +0900

    change 'wp/wp-content/plugins' working

--- scripts/Linker.php
+++ scripts/Linker.php
@@ -5,15 +5,17 @@ use Composer\Script\Event;
 
 class Linker
 {
-    public static function symlink(Event $event = null)
+    public static function postInstallWordPress(Event $event = null)
     {
+        $installedPackageName = $event->getOperation()->getPackage()->getName();
+        if ($installedPackageName !== 'wordpress') {
+            return;
+        }
+
         $projectRoot = dirname(__DIR__);
 
+        // create symlinks under wp/wp-content dir.
         $paths = [
-            [
-                'target' => "{$projectRoot}/wp-content/plugins",
-                "link" => "{$projectRoot}/wp/wp-content/my-plugins",
-            ],
             [
                 'target' => "{$projectRoot}/wp-content/mu-plugins",
                 "link" => "{$projectRoot}/wp/wp-content/my-mu-plugins",
@@ -23,11 +25,17 @@ class Linker
                 "link" => "{$projectRoot}/wp/wp-content/my-themes",
             ],
         ];
-
         foreach ($paths as $path) {
             if (!file_exists($path['link'])) {
                 symlink($path['target'], $path['link']);
             }
+        }
+
+        // create wp/wp-content/my-plugins dir.
+        $myPluginsDirPath = "{$projectRoot}/wp/wp-content/my-plugins";
+        if (!file_exists($myPluginsDirPath)) {
+            mkdir($myPluginsDirPath);
+            touch("{$myPluginsDirPath}/index.php");
         }
     }
 }
